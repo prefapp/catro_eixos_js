d8c6d2aeaf850a2fd5737787ec2d3becac3fdeab
Francisco Maseda Muiño <frmadem@gmail.com>
2017-07-06 01:01:17 +0200

    Agregados límites a los procesos

---
+++ test/fixtures/procesos/proceso_dormilon.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const CatroEixos = require("../../../index.js");
+
+class ProcesoDormilon extends CatroEixos.Proceso {
+
+ //   DEPURAR() {return true}
+
+    __r(){
+
+        return [
+            "__dormir",
+            "__apuntarResultados"
+        ]
+    }
+
+    __dormir(){
+        return new Promise((cumplida, falla) => {
+
+            setTimeout(
+
+                () => {
+
+                    this.arg("enDespertar")(this.arg("nombre"))
+
+                    cumplida()
+
+                }, 
+
+                this.arg("duerme") || 100
+            )
+        })
+    }
+
+    __apuntarResultados(){
+        this.resultado("despierto", this.arg("nombre"));
+    }
+
+}
+
+module.exports = ProcesoDormilon;
---
+++ test/test_nucleo/procesador_limites.js
@@ -0,0 +1,72 @@
+const {expect} = require("chai");
+
+const ProcesoDormilon = require("../fixtures/procesos/proceso_dormilon.js");
+
+const {Procesador} = require("../../index.js");
+
+const {Tarea} = require("../../index.js");
+
+describe("Procesador - Limites", function(){
+
+    it("SE pueden poner limites a los procesos", function(hecho){
+
+        this.timeout(50 * 1000 );
+
+        let procesador = new Procesador(
+
+            {
+                "Dormir.dormilon" : ProcesoDormilon
+            }, 
+
+            {
+
+                limites: {
+                    "Dormir.dormilon": 1
+                }
+
+        });
+
+        let dd = [];
+
+        let promesas = Array.apply(null, { length: 100 })
+
+            .map((n, i) => {
+        
+            return procesador.ejecutar(
+
+                new Tarea(
+
+                    "foo", 
+
+                    {
+                        proceso: "Dormir.dormilon", 
+    
+                        nombre: i,  
+                    
+                        duerme: 10,
+
+                        enDespertar: (n) => {
+
+                            if(n != 0){
+                                expect(dd[dd.length - 1]).to.be.equal(n -1 );
+                            }
+        
+                            dd.push(n);
+                        }
+                    }
+                )
+            )
+        })
+        
+        Promise.all(promesas)
+
+            .then((r) => {
+                hecho();
+            })
+            .catch((err) => {
+                console.log(err);
+                hecho(1);
+            })
+    })
+
+})
